647412587a8596a20aa35e091900dbfa82e0b0fe
kate <01redcat@mail.ru>
2023-12-15 00:56:55 +0300

    diplom added

--- myproject/settings.py
+++ myproject/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')#'django-insecure-(pdsz86$ac!v)@)k=cy6%5ybtez6j*njz1hzoj9%4&e^znab4e'
-
+SECRET_KEY = os.getenv('SECRET_KEY')
+#SECRET_KEY = 'django-insecure-(pdsz86$ac!v)@)k=cy6%5ybtez6j*njz1hzoj9%4&e^znab4e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False # при развертывании проекта False, в остальном True
 SESSION_COOKIE_SECURE = True
@@ -29,6 +29,8 @@ CSRF_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = [
     'krasnozhen.pythonanywhere.com',
+    '127.0.0.1',
+    '127.0.0.1:8000',
 ]
 
 INTERNAL_IPS = [
